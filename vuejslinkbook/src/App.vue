a97571f9f9fa0386c5b168cb702228119816d7e2
Quang Hiep <bacodekiller@gmail.com>
2018-07-24 00:31:20 +0700

    Connect to firebase
    Setup config

--- vuejslinkbook/src/App.vue
+++ vuejslinkbook/src/App.vue
@@ -5,14 +5,34 @@
 </template>
 
 <script>
+// connect to firebase
+import Firebase from "firebase"
+
+let config = {
+  apiKey: "AIzaSyD99-OUjCkOlydSopX1a3R9QIt6urflzoM",
+  authDomain: "vuejs-linkbooks.firebaseapp.com",
+  databaseURL: "https://vuejs-linkbooks.firebaseio.com",
+  projectId: "vuejs-linkbooks",
+  storageBucket: "",
+  messagingSenderId: "515800554311"
+};
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+
+let bookRef = db.ref('books');
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  firebase: {
+    books: bookRef
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
